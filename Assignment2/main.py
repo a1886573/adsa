57ca961605bcc933e346270eeccfcf0f95f69252
a1886573 <a1886573@adelaide.edu.au>
2025-09-12 14:24:17 +0930

    insert and find added

--- Assignment2/main.py
+++ Assignment2/main.py
@@ -2,7 +2,7 @@
 
 class AVL_Node:
     def __init__ (self,key):
-        self.key
+        self.key = key
         self.leftNode = None
         self.rightNode  = None
         self.height = 1
@@ -10,14 +10,26 @@ class AVL_Node:
 class AVL_Tree:
     def __init__(self):
         self.root = None
+    
+    def tree_Height(self,node):
+        if node is None:
+           return -1
+        
+        return 1 + max(self.tree_Height(node.leftNode), self.tree_Height(node.rightNode))
+    
+    def balance(self,node):
+        if node is None:
+            return 0
+        return self.tree_Height(node.leftNode) - self.tree_Height(node.rightNode)
+        
 
     def left_rotate(self, x):
         y = x.right
         node = y.left
         y.left = x
         x.right = node
-        y.height = 1 + max(self.getHeight(y.left), self.getHeight(y.right))
-        x.height = 1 + max(self.getHeight(x.left), self.getHeight(x.right))
+        y.height = 1 + max(self.tree_Height(y.left), self.tree_Height(y.right))
+        x.height = 1 + max(self.tree_Height(x.left), self.tree_Height(x.right))
         return y
        
        
@@ -32,11 +44,59 @@ class AVL_Tree:
         return x
     
 
-    def insert():
+    def insert(self,root,key):
+        if not root:
+            return AVL_Node(key)
+        elif key < root.key:
+            root.leftNode = self.insert(root.leftNode, key)
+        elif key > root.key:
+            root.rightNode = self.insert(root.rightNode, key)
+        else:
+            return root
+        
 
-    def delete():
+        height = self.tree_height(root)
+        balance = self.balance(root)
+        
+        # Left left rotation
+        if balance > 1 and key < root.leftNode.key:
+            return self.right_rotate(root)
+        
+        # Right right rotation
+        if balance < -1 and key > root.rightNode.key:
+            return self.left_rotate(root)
+        
+        # Left right rotation
+        if balance > 1 and key > root.leftNode.key:
+            root.leftNode = self.left_rotate(root.leftNode)
+            return self.right_rotate(root)
+        
+        #Right left rotation
+        if balance < -1 and key < root.rightNode.key:
+            root.rightNode = self.right_rotate(root.rightNode)
+            return self.left_rotate(root)
+        
+        return root
 
-    def find():
+
+
+    def find(self, root, key):
+
+        if not root:
+            return None
+        if root.key == key:
+        # node is found
+            return root
+        
+        #search in left subtree
+        if key < root.key:
+            return self.find(root.leftNode,key)
+        #if greater than search in right subtree
+        elif key > root.key:
+            return self.find(root.rightNode, key)
+        
+    def delete();
+         
 
     def preorder():
         
